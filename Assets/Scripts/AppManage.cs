efe4fe33a3c768f93a0402bbe73cd96046839793
Uchida Masatoshi <shinishimaru@gmail.com>
2020-07-12 02:13:39 +0900

    first commit

---
+++ Assets/Scripts/AppManage.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppManage : MonoBehaviour {
+
+	public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+
+		makeBall ();
+	
+	}
+
+	void makeBall(){
+
+		// make rand pos
+		Vector3 pos = new Vector3 (Random.Range (-4.0f, 4.0f), Random.Range (10.0f, 20.0f), Random.Range (-4.0f, 4.0f) );
+		GameObject b = (GameObject)Instantiate(ball,
+		            pos,
+		            Quaternion.identity);
+
+		// make rand color
+		b.GetComponent<Renderer>().material.color = new Color32( (byte)Random.Range(0,255), (byte)Random.Range(0, 255), (byte)Random.Range(0, 255), 1);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.frameCount % 5 == 0){
+			makeBall ();
+		}
+
+	}
+	
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# ReflectionProbeSimpleTest
+[![Image from Gyazo](https://i.gyazo.com/4a655394fa08843c32f815f4af4d0691.gif)](https://gyazo.com/4a655394fa08843c32f815f4af4d0691)  
+Unity Reflection Probe Simple Test
+
+# ハマった点
+<a href="https://gyazo.com/2db0c452be465849d483819f5c99cfe6"><img src="https://i.gyazo.com/2db0c452be465849d483819f5c99cfe6.jpg" alt="Image from Gyazo" width="540"/></a>  
+Reflection Probeのサイズをちゃんと調整してなくて、なかなか映り込まなかった。    
+ここは、すぐ調整した方が良いのね。  
+
+# 必要要件
+- Unity 2019.4.0f1 or later
+
+# 参考
+[Reflection Probeで鏡面反射する床を作る](http://nn-hokuson.hatenablog.com/entry/2016/10/17/202135)   
+
+# License
+[Unlicense](https://unlicense.org/)
